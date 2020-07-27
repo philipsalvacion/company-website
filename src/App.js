1580646e0ab2d7665b05661f6e137b0e27cab78e
Philip Salvacion <philipsalvacion65@gmail.com>
2020-07-27 21:58:29 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Home from './Pages/Home/Home.js';
 
 class app extends React.Component {
+	componentDidMount(){
+		alert("This is still under development");
+	}
 	render(){
 		return(
 			<div>
